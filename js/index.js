dcbaffc0ce8a54ba4aa9185850e0950e5f8d186a
jakubson7 <kijekkuba2006@gmail.com>
2022-04-10 20:26:17 +0200

    images and full menu

--- js/index.js
+++ js/index.js
@@ -2,4 +2,7 @@ const bnwPage = document.querySelector("main.bnw");
 
 bnwPage.addEventListener("click", () => {
   bnwPage.classList.add("bnw--active");
+  bnwPage.addEventListener("click", () => {
+    window.location.href = "./pages/soy-cuba-menu.html";
+  });
 });
